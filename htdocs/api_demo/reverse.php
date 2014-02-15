251b11054941be0056744d633c4eb776d661f0ce
liaohuqiu <liaohuqiu@gmail.com>
2014-02-15 10:51:59 +0800

    change request.php to reverse.php

---
+++ htdocs/api_demo/reverse.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('UTC');
+$msg = $_REQUEST['str'];
+
+$data = array();
+
+if (!$msg) {
+    $data['ok'] = 0;
+} else {
+    $data['ok'] = 1;
+}
+
+$data['result'] = strrev($msg);
+$data['server_time'] = date('Y-m-d H:i:s');
+echo json_encode($data);
