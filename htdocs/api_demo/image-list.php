a49836b2cca52c9dce264b181905d7e1addc5c41
liaohuqiu <liaohuqiu@gmail.com>
2014-08-10 01:50:22 +0800

    add image list

--- htdocs/api_demo/image-list.php
+++ htdocs/api_demo/image-list.php
@@ -1,16 +1,6 @@
 <?php
 date_default_timezone_set('UTC');
-$msg = $_REQUEST['str'];
-
 $data = array();
 
-if (!$msg) {
-    $data['ok'] = 0;
-} else {
-    $data['ok'] = 1;
-}
-
-$data['str'] = $msg;
-$data['result'] = strrev($msg);
 $data['server_time'] = date('Y-m-d H:i:s');
 echo json_encode($data);
